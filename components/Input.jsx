326370e80c5f580007302a73f5990facea449cd4
Ujen Basi <ujenbasi1122@gmail.com>
2022-10-15 10:57:29 +0545

    added todo application

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,15 +1,62 @@
-import React from "react";
+import { useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const Input = ({ onChange, inputRef, value }) => {
+const Input = ({ lists, setLists, data, setData, onEdit, editStatus }) => {
+  const onSubmit = () => {
+    if (data.title) {
+      const updatedData = [...lists, { _id: uuidv4(), ...data }];
+      setLists(updatedData);
+      localStorage.setItem("todo", JSON.stringify(updatedData));
+      setData({
+        title: "",
+        description: "",
+      });
+    }
+  };
+
+  const onChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
   return (
-    <input
-      readOnly
-      className="bg-transparent border-none font-bold text-xl text-center w-14 h-14"
-      onChange={onChange}
-      ref={inputRef}
-      type="text"
-      value={value}
-    />
+    <section className="absolute bottom-30 -mt-1 left-1/2 -translate-x-1/2 p-4 shadow-xl w-[44rem] bg-white border border-gray-300 rounded-xl">
+      <main>
+        <input
+          value={data.title}
+          onChange={onChange}
+          name="title"
+          type="text"
+          className="outline-none text-md p-2 w-full"
+          placeholder="Task name here..."
+        />
+        <textarea
+          value={data.description}
+          onChange={onChange}
+          name="description"
+          className="outline-none text-md p-2 w-full"
+          placeholder="Description"
+        />
+      </main>
+      <footer className="flex items-center mt-4 justify-end">
+        <div className="flex items-center gap-4">
+          {editStatus ? (
+            <button
+              onClick={onEdit}
+              className="text-sm px-4 py-2 bg-violet-700 hover:bg-violet-600 transition duration-150 text-white rounded-lg"
+            >
+              Update task
+            </button>
+          ) : (
+            <button
+              onClick={onSubmit}
+              className="text-sm px-4 py-2 bg-violet-700 hover:bg-violet-600 transition duration-150 text-white rounded-lg"
+            >
+              Add task
+            </button>
+          )}
+        </div>
+      </footer>
+    </section>
   );
 };
 
--- utils/handlers.js
+++
@@ -1,3 +0,0 @@
-/*
-  In this file all handlers(onChange, textShortner, etc), async requests and other functions are defined, *axios* is already installed
-*/
